0f248195ff1ff54a78cf3b3a70510c4273cf51d0
IMRANDIL <aliimranadil2@gmail.com>
2023-09-02 19:58:10 +0530

    just tested this factorial program with bigger number in my machine, ha ha ha

--- main.go
+++ main.go
@@ -185,7 +185,7 @@ import (
 
 func main() {
 	// Define the number for which we want to calculate the factorial.
-	num := 5
+	num := 6
 
 	// Create a channel to collect results from goroutines.
 	resultChan := make(chan *big.Int, num)
